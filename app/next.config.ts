57cc3e5f8c8ef4309024d82484ef1550343fb858
Hans Imberg <hans@imberg.dev>
2026-08-18 21:17:35 +0300

    chore(next): scope output file tracing root

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingRoot: path.join(__dirname),
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
